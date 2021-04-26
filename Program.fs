020467086273b748d1c3496a3b7bdc4dbe1df5ea
Kazuma Arino <hogeika2@gmail.com>
2021-04-27 08:30:23 +0900

    Remove dev mode and use samples sh script for F5.

--- Program.fs
+++ Program.fs
@@ -4,33 +4,17 @@ open Argu
 
 type Arguments =
     | [<AltCommandLine("-d")>] Debug
-    | [<AltCommandLine("-dev")>] Dev_Mode
     | [<MainCommand; ExactlyOnce; Last>] File of file:string
 with
     interface IArgParserTemplate with
         member arg.Usage =
             match arg with
             | Debug -> "Debug mode. Show log and keep intemediate tmp folder."
-            | Dev_Mode -> "Development mode. Use when launch from VS Code with F5."
             | File _-> "Guash script file."
 
-let debugInput () =
-    let target = """#!~/bin/guash
 
-ls -t ~/work/GitHub/karino2.github.io/_posts | guash_filter "Src file"
-guash_readtext "Dest name"
-guash_doquery SRC DEST
-
-ln ~/work/GitHub/karino2.github.io/_posts/${SRC} ~/Google\ ドライブ/DriveText/TeFWiki/${DEST}.md
-"""
-    target.Split("\n", StringSplitOptions.TrimEntries)
-
-
-let getInput isDev path =
-    if isDev then
-        debugInput ()
-    else
-        File.ReadAllLines(path)
+let getInput path =
+    File.ReadAllLines(path)
 
 
 let runGuash lines =
@@ -49,22 +33,19 @@ let runGuash lines =
 
 [<EntryPoint>]
 let main argv =
-
     let parser = ArgumentParser.Create<Arguments>(programName = "guash")
     try
         let results = parser.Parse argv
         let arg = results.GetAllResults()
 
-        let mutable isDev = false
         let mutable path = ""
 
         match arg with
         |Debug::_ -> Common.debugMode <- true
-        |Dev_Mode::_ -> isDev <- true
         |(File f)::_ -> path <- f
         |_->()
 
-        let lines = getInput isDev path
+        let lines = getInput path
         runGuash lines
         0 // return an integer exit code
     with
