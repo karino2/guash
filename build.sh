9e969b820a158969bf3fa8133fcb448f3c8403b4
Kazuma Arino <hogeika2@gmail.com>
2022-02-18 19:52:59 +0900

    update dotnet to 6.0

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 # dotnet publish -c release -r osx-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishTrimmed=true
 dotnet publish -c release -r osx-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-cp bin/release/net5.0/osx-x64/publish/guash_doquery ~/bin
+cp bin/release/net6.0/osx-x64/publish/guash_doquery ~/bin
