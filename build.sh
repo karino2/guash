769e53cd855a0680a23d0a6c0b3e2251c5f84fde
Kazuma Arino <hogeika2@gmail.com>
2025-11-04 14:50:35 +0900

    Fix for Apple silicon.

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 # dotnet publish -c release -r osx-x64 /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true /p:PublishTrimmed=true
 # dotnet publish -c release -r osx-x64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-dotnet publish -c release -r osx-amd64 --self-contained false /p:PublishSingleFile=true /p:IncludeNativeLibrariesForSelfExtract=true
-cp bin/release/net9.0/osx-amd64/publish/guash_doquery ~/bin
+dotnet publish -c release -r osx-arm64 --self-contained false /p:PublishSingleFile=true /p:IncludeAllContentForSelfExtract=true
+cp bin/release/net9.0/osx-arm64/publish/guash_doquery ~/bin
